6de144b8b3f9ab5ea32181829f4a25d8f2396208
isaacszf <isaacsouzafernandes1@hotmail.com>
2024-01-10 04:01:24 -0300

    feat: updated header

--- src/components/global/Header/Header.tsx
+++ src/components/global/Header/Header.tsx
@@ -1,4 +1,6 @@
+import { FaGithub, FaInstagram } from 'react-icons/fa6'
 import Link from 'next/link'
+import Image from 'next/image'
 
 import SizedContainer from '../SizedContainer/SizedContainer'
 
@@ -8,16 +10,26 @@ export default function Header() {
   return (
     <header className={styles.header}>
       <SizedContainer className={styles.container}>
-        <Link href="/" className={`${styles.link} ${styles.titleLink}`}>
+        <Link href="/" className={styles.title}>
+          <Image src="/icon.png" alt="icon" width={32} height={32} />
           <h3>antiqbras</h3>
         </Link>
 
-        <div>
-          <Link
-            href="/discover"
-            className={`${styles.link} ${styles.discoverLink}`}
-          >
-            <span>Discover</span>
+        <div className={styles.links}>
+          <Link href="/discover">
+            <span>Mapa</span>
+          </Link>
+          <Link href="/blog">
+            <span>Blog</span>
+          </Link>
+        </div>
+
+        <div className={styles.socials}>
+          <Link href="">
+            <FaInstagram />
+          </Link>
+          <Link href="https://github.com/AntiqBras/AntiqBras/">
+            <FaGithub />
           </Link>
         </div>
       </SizedContainer>
